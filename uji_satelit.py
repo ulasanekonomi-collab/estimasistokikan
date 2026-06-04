fe5d3c22635ba0255d86e889fae681a90db833f5
ulasanekonomi-collab <ulasanekonomi@gmail.com>
2026-06-05 06:32:23 +0700

    Change text alignment from center to left

--- uji_satelit.py
+++ uji_satelit.py
@@ -119,7 +119,7 @@ with st.sidebar:
     
     # 3. Menampilkan teks identitas
     st.markdown("""
-    <div style="text-align: center;">
+    <div style="text-align: left;">
         <p style="font-size: 12px; color: blue;">Yuhka Sundaya</p>
         <p style="font-size: 12px; color: gray;">Ekonomi Pembangunan | Unisba | 2026</p>
     </div>
