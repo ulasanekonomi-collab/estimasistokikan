205107283bb5a020ed0ee6fc22991a7165ed5ba2
ulasanekonomi-collab <ulasanekonomi@gmail.com>
2026-06-05 06:29:26 +0700

    Reduce image widths in sidebar

--- uji_satelit.py
+++ uji_satelit.py
@@ -112,10 +112,10 @@ import streamlit as st
 with st.sidebar:
     # 1. Menampilkan Logo Unisba (Pastikan file 'logounisba.png' ada di folder yang sama)
     # Jika belum ada file, bisa lewatkan baris ini atau ganti dengan link URL
-    st.image("https://upload.wikimedia.org/wikipedia/commons/9/9e/Lambang-Universitas_Islam_Bandung.png", width=150)
+    st.image("https://upload.wikimedia.org/wikipedia/commons/9/9e/Lambang-Universitas_Islam_Bandung.png", width=75)
     
     # 2. Menampilkan Foto Akang (Pastikan file 'foto_yuhka.jpg' ada di folder)
-    st.image("yuka.png", width=100) 
+    st.image("yuka.png", width=50) 
     
     # 3. Menampilkan teks identitas
     st.markdown("""
